081bc8a47bafffcaa084b418d2d0086e37bbc4f2
Matt Smith <msmith21794@gmail.com>
2026-01-29 22:51:06 -0500

    removing 0.1.0 docs

--- src/data/versions.ts
+++ src/data/versions.ts
@@ -10,10 +10,6 @@ export const VERSIONS: Version[] = [
     label: "v0.2.0",
     default: true,
   },
-  {
-    tag: "0.1.1",
-    label: "v0.1.1",
-  },
 ];
 
 export const CURRENT_VERSION = "dev";
